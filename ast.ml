f5a7a901a3313bf2208701f870645c97700e9d3d
Nancy Xu <nancyxu1996@gmail.com>
2017-03-14 10:40:11 -0400

    finished ast with much comments/questions

--- ast.ml
+++ ast.ml
@@ -1,31 +1,66 @@
 (* Abstract Syntax Tree and functions for printing it *)
 
 type op = Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq |
-          And | Or
+          And | Or 
+
+type nop = AccessNode (*underscore, e.g. g1_n1*) | AccessNodeField (* At, g1_n1@visited*)
+(*check with TA *)
+
+type gop = AddNode | RemoveNode | AddEdge | RemoveEdge (*check with TA *)
+
+type sop = AccessStructField (* Dot *)
+(* check with TA *)
 
 type uop = Neg | Not
 
-type typ = Int | Bool | Void
+type typ = Int | Bool | Void | String | Float | Node | Graph | List | Queue | PQueue | Map | Struct | Null
 
 type bind = typ * string
 
+(* Do we need to add in expressions for accessing Collections elements? *)
+(* Do we need to add in an expression representing the (string, int) pair value for the Map collection? *)
 type expr =
-    Literal of int
+  IntLit of int
+
+  | FloatLit of float 
+  | StringLit of string
   | BoolLit of bool
   | Id of string
+  | ListLit of expr list 
+  | QueueLit of expr list
+  | PQueueLit of expr list
+  | MapLit of (expr * expr) list
+  | StructLit of expr list 
+  | NodeLit of string * expr * expr * expr * expr
+  (* check with TA whether expr has to be more specific, such as boollit, etc. *)
+  | GraphLit of NodeLit list 
+  (*check with TA *)
   | Binop of expr * op * expr
   | Unop of uop * expr
+  | StructOp of expr * sop * expr
+  (* check with TA *)
+  | NodeOp of expr * nop * expr
+  | GraphOp of expr * gop * expr
   | Assign of string * expr
   | Call of string * expr list
   | Noexpr
 
 type stmt =
     Block of stmt list
+
+
   | Expr of expr
   | Return of expr
   | If of expr * stmt * stmt
   | For of expr * expr * expr * stmt
+  | For of expr * expr * stmt (* check with TA the correctness of expr
+     whether you use stmt/stmt list*)
+  (*for of string * expr * stmt *)
+  (* should we also replace expr with Nodelit or graphlit? *)
   | While of expr * stmt
+  (*should we add statements for declaring collections, structs, nodes, or graphs? *)
+  (*for example, should we use something like NodeDef, GraphDef, etc.*)
+  
 
 type func_decl = {
     typ : typ;
@@ -36,68 +71,3 @@ type func_decl = {
   }
 
 type program = bind list * func_decl list
-
-(* Pretty-printing functions *)
-
-let string_of_op = function
-    Add -> "+"
-  | Sub -> "-"
-  | Mult -> "*"
-  | Div -> "/"
-  | Equal -> "=="
-  | Neq -> "!="
-  | Less -> "<"
-  | Leq -> "<="
-  | Greater -> ">"
-  | Geq -> ">="
-  | And -> "&&"
-  | Or -> "||"
-
-let string_of_uop = function
-    Neg -> "-"
-  | Not -> "!"
-
-let rec string_of_expr = function
-    Literal(l) -> string_of_int l
-  | BoolLit(true) -> "true"
-  | BoolLit(false) -> "false"
-  | Id(s) -> s
-  | Binop(e1, o, e2) ->
-      string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
-  | Unop(o, e) -> string_of_uop o ^ string_of_expr e
-  | Assign(v, e) -> v ^ " = " ^ string_of_expr e
-  | Call(f, el) ->
-      f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
-  | Noexpr -> ""
-
-let rec string_of_stmt = function
-    Block(stmts) ->
-      "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
-  | Expr(expr) -> string_of_expr expr ^ ";\n";
-  | Return(expr) -> "return " ^ string_of_expr expr ^ ";\n";
-  | If(e, s, Block([])) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s
-  | If(e, s1, s2) ->  "if (" ^ string_of_expr e ^ ")\n" ^
-      string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2
-  | For(e1, e2, e3, s) ->
-      "for (" ^ string_of_expr e1  ^ " ; " ^ string_of_expr e2 ^ " ; " ^
-      string_of_expr e3  ^ ") " ^ string_of_stmt s
-  | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
-
-let string_of_typ = function
-    Int -> "int"
-  | Bool -> "bool"
-  | Void -> "void"
-
-let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n"
-
-let string_of_fdecl fdecl =
-  string_of_typ fdecl.typ ^ " " ^
-  fdecl.fname ^ "(" ^ String.concat ", " (List.map snd fdecl.formals) ^
-  ")\n{\n" ^
-  String.concat "" (List.map string_of_vdecl fdecl.locals) ^
-  String.concat "" (List.map string_of_stmt fdecl.body) ^
-  "}\n"
-
-let string_of_program (vars, funcs) =
-  String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
-  String.concat "\n" (List.map string_of_fdecl funcs)
