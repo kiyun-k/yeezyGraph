8aaf521b775686f197b8c3f7ff02673591e2168b
Nancy Xu <nancyxu1996@gmail.com>
2017-05-10 18:55:54 -0400

    finished

--- yeezygraph.sh
+++ yeezygraph.sh
@@ -7,3 +7,9 @@ clang -emit-llvm -o queue.bc -c c/queue.c
 clang -emit-llvm -o map.bc -c c/map.c
 clang -emit-llvm -o pqueue.bc -c c/pqueue.c
 
+./yeezygraph.native <$1> a.ll
+llvm-link linkedlist.bc node.bc graph.bc queue.bc map.bc pqueue.bc a.ll -S > run.ll
+./a.out
+rm a.ll
+rm run.ll
+rm ./a.out
