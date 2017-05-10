cb8ce203219f1258e599e0ebb1db70bc371330e8
Nancy Xu <nancyxu1996@gmail.com>
2017-05-10 18:58:00 -0400

    finish

--- generateLLVMCode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-clang -emit-llvm -o linkedlist.bc -c c/linkedlist.c
-clang -emit-llvm -o node.bc -c c/node.c
-clang -emit-llvm -o graph.bc -c c/graph.c
-clang -emit-llvm -o queue.bc -c c/queue.c
-clang -emit-llvm -o map.bc -c c/map.c
-clang -emit-llvm -o pqueue.bc -c c/pqueue.c
